aa017363b51e35fd1fafc505ef43b86c7fc098e9
Christian Weck <weck.chr@gmail.com>
2022-12-04 19:10:46 +0100

    Changed OrderRepo internal Map to Map<Integer, LinkedList<Order>>

--- src/main/java/InterfaceShop/repo/OrderRepo.java
+++ src/main/java/InterfaceShop/repo/OrderRepo.java
@@ -5,7 +5,6 @@ package InterfaceShop.repo;
 import InterfaceShop.model.Order;
 import InterfaceShop.model.Product;
 import InterfaceShop.service.HCG;
-import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 import java.util.HashMap;
@@ -15,24 +14,37 @@ import java.util.Map;
 
 @NoArgsConstructor
 public class OrderRepo {
-    public Map<Integer, Order> orderMap = new HashMap<>();
-    public Map<Integer, LinkedList<Order>> orderMapL = new HashMap<>();
+    public Map<Integer, LinkedList<Order>> orderMap = new HashMap<>();
 
     public void addOrder(List<Product> productList) {
         int hashCode = new HCG("Unknown recipient").getHCG();
         Order newOrder = new Order("Unknown recipient", productList);
-        if (this.orderMapL.get(hashCode) == null) {
-            this.orderMapL.put(hashCode, new LinkedList<>());
-            this.orderMapL.get(hashCode).add(newOrder);
+        if (this.orderMap.get(hashCode) == null) {
+            this.orderMap.put(hashCode, new LinkedList<>());
+            this.orderMap.get(hashCode).add(newOrder);
+        } else {
+            this.orderMap.get(hashCode).add(newOrder);
         }
 
     }
 
     public void addOrder(Order order) {
-        this.orderMap.put(new HCG(order.getRecipient()).getHCG(), order );
+        int hashCode = new HCG(order.getRecipient()).getHCG();
+        if (this.orderMap.get(hashCode) == null) {
+            this.orderMap.put(hashCode, new LinkedList<>());
+            this.orderMap.get(hashCode).add(order);
+        } else {
+            this.orderMap.get(hashCode).add(order);
+        }
     }
     public void addOrder(int ID, Order order) {
-        this.orderMap.put(ID, order);
+        if (this.orderMap.get(ID) == null) {
+            this.orderMap.put(ID, new LinkedList<>());
+            this.orderMap.get(ID).add(order);
+        } else {
+            this.orderMap.get(ID).add(order);
+        }
+
     }
 
 
--- src/test/java/InterfaceShop/repo/OrderRepoTest.java
+++ src/test/java/InterfaceShop/repo/OrderRepoTest.java
@@ -5,13 +5,9 @@ import InterfaceShop.service.HCG;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class OrderRepoTest {
 
@@ -20,7 +16,7 @@ class OrderRepoTest {
         return Stream.of(new Bakery(), new Diary(),new Vegetable()).collect(Collectors.toList());
     }
     Order generateTestOrder() {
-        return new Order("Christian", generateTestProductList());
+        return new Order("Test Recipient", generateTestProductList());
     }
     List<Order> generateTestOrderList() {
         return Stream.of(generateTestOrder()).collect(Collectors.toList());
@@ -31,9 +27,9 @@ class OrderRepoTest {
         //Given
         OrderRepo testOrderRepo = new OrderRepo();
         //When
-        Map<Integer, Order> actual= testOrderRepo.orderMap;
+        Map<Integer, LinkedList<Order>> actual= testOrderRepo.orderMap;
         //Then
-        Assertions.assertEquals(new HashMap<Integer, Order>(), actual);
+        Assertions.assertEquals(new HashMap<Integer, LinkedList<Order>>(), actual);
     }
 
     @Test
@@ -42,44 +38,101 @@ class OrderRepoTest {
         OrderRepo testOrderRepo = new OrderRepo();
         testOrderRepo.addOrder(1, generateTestOrder());
 
-        Map<Integer, Order> newTestOrderMap = new HashMap<>();
-        newTestOrderMap.put(1, generateTestOrder());
+        Map<Integer, LinkedList<Order>> newTestOrderMap = new HashMap<>();
+        newTestOrderMap.put(1, new LinkedList<>());
+        newTestOrderMap.get(1).add(generateTestOrder());
         //When
-        Map<Integer, Order> actual= testOrderRepo.orderMap;
+        Map<Integer, LinkedList<Order>> actual= testOrderRepo.orderMap;
         //Then
         Assertions.assertEquals(newTestOrderMap, actual);
     }
 
     @Test
-    void orderRepo_addOrderToMap() {
+    void orderRepo_addOrder_CheckCorrectSizeOfLinkedList() {
         //Given
         OrderRepo testOrderRepo = new OrderRepo();
+        testOrderRepo.addOrder(1, generateTestOrder());
+
+        //When
+        int actual= testOrderRepo.orderMap.get(1).size();
+        //Then
+        Assertions.assertEquals(1, actual);
+    }
+
+    @Test
+    void orderRepo_addOrderToMap_WhenLinkedListAtHashCodeIsEmpty() {
+        //Given
+        OrderRepo testOrderRepo = new OrderRepo();
+        int testRecipientKey = new HCG("Test Recipient").getHCG();
         testOrderRepo.addOrder(generateTestOrder());
 
-        Map<Integer, Order> newTestOrderMap = new HashMap<>();
-        newTestOrderMap.put(new HCG("Christian").getHCG(), generateTestOrder());
         //When
-        Map<Integer, Order> actual= testOrderRepo.orderMap;
+        Order actual = testOrderRepo.orderMap.get(testRecipientKey).get(0);
         //Then
-        Assertions.assertEquals(newTestOrderMap, actual);
+        Assertions.assertEquals(generateTestOrder(), actual);
     }
 
     @Test
-    void orderRepo_addOrderFromProductList() {
+    void orderRepo_addOrderToMap_WhenLinkedListAtHashCodeExists() {
         //Given
         OrderRepo testOrderRepo = new OrderRepo();
-        testOrderRepo.addOrder(generateTestProductList());
+        int testRecipientKey = new HCG("Test Recipient").getHCG();
+        testOrderRepo.addOrder(generateTestOrder());
+        testOrderRepo.addOrder(generateTestOrder());
 
-        Map<Integer, Order> newTestOrderMap = new HashMap<>();
-        newTestOrderMap.put(
-                new HCG("Unknown recipient").getHCG(),
-                new Order("Unknown recipient", generateTestProductList())
-        );
         //When
-        Map<Integer, Order> actual= testOrderRepo.orderMap;
+        Order actual = testOrderRepo.orderMap.get(testRecipientKey).get(1);
         //Then
-        Assertions.assertEquals(newTestOrderMap, actual);
+        Assertions.assertEquals(generateTestOrder(), actual);
     }
 
 
+
+    @Test
+    void orderRepo_addOrderFromProductList_AddedToEmptyLinkedList_UnderCorrectKey() {
+        //Given
+        OrderRepo testOrderRepo = new OrderRepo();
+        int unknownRecipientKey = new HCG("Unknown recipient").getHCG();
+        testOrderRepo.addOrder(generateTestProductList());
+        //When
+        Order actual = testOrderRepo.orderMap.get(unknownRecipientKey).get(0);
+        //Then
+        Assertions.assertEquals(new Order("Unknown recipient", generateTestProductList()), actual);
+    }
+
+    @Test
+    void orderRepo_addOrderFromProductList_AddedToExistingLinkedList_UnderCorrectKey() {
+        //Given
+        OrderRepo testOrderRepo = new OrderRepo();
+        int unknownRecipientKey = new HCG("Unknown recipient").getHCG();
+        testOrderRepo.addOrder(generateTestProductList());
+        testOrderRepo.addOrder(generateTestProductList());
+        //When
+        Order actual = testOrderRepo.orderMap.get(unknownRecipientKey).get(1);
+        //Then
+        Assertions.assertEquals(new Order("Unknown recipient", generateTestProductList()), actual);
+    }
+
+    @Test
+    void orderRepo_addOrder_WithID_ToEmptyLinkedList() {
+        //Given
+        OrderRepo testOrderRepo = new OrderRepo();
+        testOrderRepo.addOrder(1, generateTestOrder());
+        //When
+        Order actual = testOrderRepo.orderMap.get(1).get(0);
+        //Then
+        Assertions.assertEquals(new Order("Test Recipient", generateTestProductList()), actual);
+    }
+
+    @Test
+    void orderRepo_addOrder_WithID_ToExistingLinkedList() {
+        //Given
+        OrderRepo testOrderRepo = new OrderRepo();
+        testOrderRepo.addOrder(1, generateTestOrder());
+        testOrderRepo.addOrder(1, generateTestOrder());
+        //When
+        Order actual = testOrderRepo.orderMap.get(1).get(1);
+        //Then
+        Assertions.assertEquals(new Order("Test Recipient", generateTestProductList()), actual);
+    }
 }
